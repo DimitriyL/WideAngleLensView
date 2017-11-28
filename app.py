3b343e63b42fa4bff1c1cdea96a179605f06512a
MrEricL <eli6@stuy.edu>
2017-11-27 23:13:58 -0500

    changes

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
+
 # -*- coding: utf-8 -*-
 from flask import Flask, render_template
-import json, urllib2
+import json, urllib2, sys
 from utils import watson, guardian
 
 # object = urllib2.urlopen("https://content.guardianapis.com/search?api-key=b108bad9-9e7b-4afb-829b-1a9a19aef420")
 # string = object.read()
 # d = json.loads(string)
 
+#for the unicode stuff to work in Jijna
+if sys.version_info.major < 3:
+    reload(sys)
+sys.setdefaultencoding('utf8')
+
+#to make sure the dicts turn null
 def checkNullDict(d):
 	if len(d['tones']) == 0:
 		return {'tones':'None','scores':0}
@@ -19,9 +26,6 @@ def addEmotionSent(arr):
    	for story in arr:
 		story['emotion'] = watson.mainEmotion(story['text'])
 	return arr
-
-#articleList = guardian.headlines()[0]
-#print articleList
 
 '''
 Return Type  
@@ -36,24 +40,55 @@ PRIMARY: LIST of DICTS
 	...
 ]
 '''
-#masterList[0]['text'].encode('utf-8')
+'''
+Splicer: Now Useless
+def splicer(s):
+	s.
+	ret = [] #should be # of times it runs
+
+	bArt = len(s.encode('utf-8')) #bytes of the article
+	if  bArt > 9500:
+		repeatRun = bArt/9500 #how many subsections
+		iterator = repeatRun
+		lower = 0
+		upper = len(s)/repeatRun
+		print s[0:1]
+		return "hi"
+		while iterator > 0:
+			place = s[upper:upper+1] #curr section
+			while place != "." and place != "?" and place != "!":
+				print upper
+				print place
+				print "\n\n"
+				upper+=1
+			ret.append(s[lower:upper])
+			lower = upper
+			iterator = iterator - 1
+		ret.append(s[lower:len(s)-1])
+		return ret
+	else:
+		ret.append(s)
+		return ret
+'''
+
+
 def theGrandPizzah (arr):
 	masterList = []
         #print arr
 	for each in arr:
                 #print each['title']
 		tempDict={}
-		tempDict['title'] = each['title']
-                #print watson.mainEmotion(each['title'])
-		tempDict['titleEmotions'] = checkNullDict(watson.mainEmotion(each['title']))
-		tempDict['text'] = each['text']
+
 		try:
 			tempDict['textEmotion'] = watson.sentEmotion(each['text'])
+			tempDict['title'] = each['title']
+			tempDict['titleEmotions'] = checkNullDict(watson.mainEmotion(each['title']))
+			masterList.append(tempDict)
 		except:
-			print "hmm"
-		masterList.append(tempDict)
+			print "Nice!"
+		
 
-	print masterList
+	#print masterList
 	return masterList
 
 articleList = theGrandPizzah(guardian.headlines())
